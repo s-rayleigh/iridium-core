df1b986ee2a1374a0da4ac5844722f74d1b4b0ad
s-rayleigh <rayleigh@protonmail.ch>
2018-03-20 17:35:17 +0200

    Removed some deprecated constants

--- core/constants.php
+++ core/constants.php
@@ -27,7 +27,7 @@
  * Iridium Core version.
  * Format: <major version>.<minor version>[.<patch>][-alpha|beta|indev]
  */
-define('IRIDIUM_CORE_VERSION', '0.1-indev');
+define('IRIDIUM_VERSION', '0.1-indev');
 
 //===============================- PATHS -===============================
 
@@ -36,12 +36,6 @@ define('IRIDIUM_CORE_VERSION', '0.1-indev');
  * @deprecated
  */
 define('ROOT_PATH', realpath(dirname(__FILE__)) . '/../');
-
-/**
- * Путь к директории кеша сайта.
- * @deprecated
- */
-define('CACHE_PATH', 'storage/cache/');
 
 /**
  * Путь к хранилищу созданных или загруженных файлов.
@@ -83,11 +77,6 @@ define('TIMESTAMP', time());
 define('TIME_FORMAT', 'd.m.Y H:i:s');
 
 /**
- * @deprecated
- */
-define('HTTPS', false);
-
-/**
  * Session lifetime.
  * 12*60*60=43200, etc 12 hrs.
  */
@@ -112,35 +101,5 @@ define('LOGGING_ENABLED', true);
  * Current logging level.
  */
 define('LOG_LEVEL', 5);
-
-//=======================================================================
-
-//==============================- DAEMONS -==============================
-
-/**
- * Режим отладки демонов.
- * В данном режиме stderr демона перенаправляется в файл daemon_<name>_err.log.
- */
-define('DAEMON_DEBUG_MODE', true);
-
-//Коды результатов
-define('DAEMON_OP_SUCCESS',				0);
-define('DAEMON_NO_FILE',				1);
-define('DAEMON_START_PROCESS_ERROR',	3);
-define('DAEMON_UNDEFINED_COMMAND',		4);
-define('DAEMON_NO_NAME',				5);
-define('DAEMON_NO_IN_DB',				6);
-define('DAEMON_ALREADY_LAUNCHED',		7);
-define('DAEMON_NO_CLASS',				8);
-define('DAEMON_NOT_RUNNING',			9);
-
-//Коды статуса демона
-define('DAEMON_DISABLED', 0);	//Отключен
-define('DAEMON_WAITING', 1);	//Ждет указанное в БД время
-define('DAEMON_WORKING', 2);	//Работает
-
-//Регулярные выражения проверки названий демона
-define('DAEMON_NAME_REGEXPR', '^[A-z_]{2,30}$');
-define('DAEMON_DISPL_NAME_REGEXPR', '^[A-Za-zА-Яа-я0-9_\s\-]{2,30}$');
 
 //=======================================================================
